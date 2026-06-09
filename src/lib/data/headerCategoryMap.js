3769132f8596f90acf5dfc4b41fc4cb440f8afe5
Hassan Aliyu <hassaniliyasu800@gmail.com>
2026-06-09 18:30:53 +0100

    Added perfumes

--- src/lib/data/headerCategoryMap.js
+++ src/lib/data/headerCategoryMap.js
@@ -4,10 +4,10 @@ const headerCategoryMap = {
   Women: "Women's Fashion",
   Unisex: "Unisex Fashion",
   Accessories: "Accessories",
-  Fabrics: "Fabrics & Materials",
+  // Fabrics: "Fabrics & Materials",
   // Kids: "Kids Fashion",
   // Baby: "Baby & Maternity",
-  // Beauty: "Beauty & Personal Care",
+  Perfumes: "Beauty & Personal Care",
   // Home: "Home & Lifestyle",
   // Art: "Art & Custom Crafts",
   // Gifts: "Gift Shops"
